a607e1212b5498e99a25edbd1512bd3223f1b9db
Tubagus Adhitya Permana <aditt7116@gmail.com>
2021-06-28 11:00:28 +0700

    first commit

---
+++ app/src/main/java/com/tbadhit/myrecyclerview/Hero.kt
@@ -0,0 +1,7 @@
+package com.tbadhit.myrecyclerview
+
+data class Hero(
+    var name: String = "",
+    var detail: String = "",
+    var photo: Int = 0
+)
---
+++ app/src/main/java/com/tbadhit/myrecyclerview/ListHeroAdapter.kt
@@ -0,0 +1,55 @@
+package com.tbadhit.myrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    private lateinit var onItemClickCallBack: OnItemClickCallBack
+
+
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        var tvDetail: TextView = itemView.findViewById(R.id.tv_item_detail)
+        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_row_hero, viewGroup, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val hero = listHero[position]
+
+        Glide.with(holder.itemView.context)
+            .load(hero.photo)
+            .apply(RequestOptions().override(55, 55))
+            .into(holder.imgPhoto)
+
+        holder.tvName.text = hero.name
+        holder.tvDetail.text = hero.detail
+
+        holder.itemView.setOnClickListener {
+            onItemClickCallBack.onItemClicked(listHero[holder.adapterPosition])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listHero.size
+    }
+
+    interface OnItemClickCallBack {
+        fun onItemClicked(data: Hero)
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallBack) {
+        this.onItemClickCallBack = onItemClickCallback
+    }
+}
